37d18b405e0c2f24bf54b2c839a908a6fd8468e8
Inglsmit <paul.inglsmit@gmail.com>
2022-04-04 00:39:02 +0300

    72 sortable items

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useState, useRef } from '@wordpress/element';
 import {
 	useBlockProps,
 	RichText,
@@ -10,6 +10,7 @@ import {
 } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
+import { usePrevious } from '@wordpress/compose';
 import { isBlobURL, revokeBlobURL } from '@wordpress/blob';
 import {
 	Spinner,
@@ -18,178 +19,338 @@ import {
 	PanelBody,
 	TextareaControl,
 	SelectControl,
+	Icon,
+	Tooltip,
+	TextControl,
+	Button,
 } from '@wordpress/components';
+import {
+	DndContext,
+	useSensor,
+	useSensors,
+	PointerSensor,
+} from '@dnd-kit/core';
+import {
+	SortableContext,
+	horizontalListSortingStrategy,
+	arrayMove,
+} from '@dnd-kit/sortable';
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
+import SortableItem from './sortable-item';
 
-function Edit({ attributes, setAttributes, noticeOperations, noticeUI }) {
-	const { name, bio, url, alt, id } = attributes;
-	const [blobURL, setBlobURL] = useState();
+function Edit( {
+	attributes,
+	setAttributes,
+	noticeOperations,
+	noticeUI,
+	isSelected,
+} ) {
+	const { name, bio, url, alt, id, socialLinks } = attributes;
+	const [ blobURL, setBlobURL ] = useState();
+	const [ selectedLink, setSelectedLink ] = useState();
 
-	const imageObject = useSelect(
-		(select) => {
-			const { getMedia } = select('core');
-			return id ? getMedia(id) : null;
-		},
-		[id]
+	const prevURL = usePrevious( url );
+	const prevIsSelected = usePrevious( isSelected );
+
+	const sensors = useSensors(
+		useSensor( PointerSensor, {
+			activationConstraint: { distance: 5 },
+		} )
 	);
 
-	const imageSizes = useSelect((select) => {
-		return select(blockEditorStore).getSettings().imageSizes;
-	}, []);
+	const titleRef = useRef();
+
+	const imageObject = useSelect(
+		( select ) => {
+			const { getMedia } = select( 'core' );
+			return id ? getMedia( id ) : null;
+		},
+		[ id ]
+	);
+
+	const imageSizes = useSelect( ( select ) => {
+		return select( blockEditorStore ).getSettings().imageSizes;
+	}, [] );
 
 	const getImageSizeOptions = () => {
-		if (!imageObject) return [];
+		if ( ! imageObject ) return [];
 		const options = [];
 		const sizes = imageObject.media_details.sizes;
-		for (const key in sizes) {
-			const size = sizes[key];
-			const imageSize = imageSizes.find((s) => s.slug === key);
-			if (imageSize) {
-				options.push({
+		for ( const key in sizes ) {
+			const size = sizes[ key ];
+			const imageSize = imageSizes.find( ( s ) => s.slug === key );
+			if ( imageSize ) {
+				options.push( {
 					label: imageSize.name,
 					value: size.source_url,
-				});
+				} );
 			}
 		}
 		return options;
 	};
 
-	const onChangeName = (newName) => {
-		setAttributes({ name: newName });
+	const onChangeName = ( newName ) => {
+		setAttributes( { name: newName } );
 	};
-	const onChangeBio = (newBio) => {
-		setAttributes({ bio: newBio });
+	const onChangeBio = ( newBio ) => {
+		setAttributes( { bio: newBio } );
 	};
-	const onChangeAlt = (newAlt) => {
-		setAttributes({ alt: newAlt });
+	const onChangeAlt = ( newAlt ) => {
+		setAttributes( { alt: newAlt } );
 	};
-	const onSelectImage = (image) => {
-		if (!image || !image.url) {
-			setAttributes({ url: undefined, id: undefined, alt: '' });
+	const onSelectImage = ( image ) => {
+		if ( ! image || ! image.url ) {
+			setAttributes( { url: undefined, id: undefined, alt: '' } );
 			return;
 		}
-		setAttributes({ url: image.url, id: image.id, alt: image.alt });
+		setAttributes( { url: image.url, id: image.id, alt: image.alt } );
 	};
-	const onSelectURL = (newURL) => {
-		setAttributes({
+	const onSelectURL = ( newURL ) => {
+		setAttributes( {
 			url: newURL,
 			id: undefined,
 			alt: '',
-		});
+		} );
 	};
-	const onChangeImageSize = (newURL) => {
-		setAttributes({ url: newURL });
+	const onChangeImageSize = ( newURL ) => {
+		setAttributes( { url: newURL } );
 	};
-	const onUploadError = (message) => {
+	const onUploadError = ( message ) => {
 		noticeOperations.removeAllNotices();
-		noticeOperations.createErrorNotice(message);
+		noticeOperations.createErrorNotice( message );
 	};
 
 	const removeImage = () => {
-		setAttributes({
+		setAttributes( {
 			url: undefined,
 			alt: '',
 			id: undefined,
-		});
+		} );
 	};
 
-	useEffect(() => {
-		if (!id && isBlobURL(url)) {
-			setAttributes({
+	const addNewSocialItem = () => {
+		setAttributes( {
+			socialLinks: [ ...socialLinks, { icon: 'wordpress', link: '' } ],
+		} );
+		setSelectedLink( socialLinks.length );
+	};
+
+	const updateSocialItem = ( type, value ) => {
+		const socialLinksCopy = [ ...socialLinks ];
+		socialLinksCopy[ selectedLink ][ type ] = value;
+		setAttributes( { socialLinks: socialLinksCopy } );
+	};
+
+	const removeSocialItem = () => {
+		setAttributes( {
+			socialLinks: [
+				...socialLinks.slice( 0, selectedLink ),
+				...socialLinks.slice( selectedLink + 1 ),
+			],
+		} );
+		setSelectedLink();
+	};
+
+	const handleDragEnd = ( event ) => {
+		const { active, over } = event;
+		if ( active && over && active.id !== over.id ) {
+			const oldIndex = socialLinks.findIndex(
+				( i ) => active.id === `${ i.icon }-${ i.link }`
+			);
+			const newIndex = socialLinks.findIndex(
+				( i ) => over.id === `${ i.icon }-${ i.link }`
+			);
+			setAttributes( {
+				socialLinks: arrayMove( socialLinks, oldIndex, newIndex ),
+			} );
+			setSelectedLink( newIndex );
+		}
+	};
+
+	useEffect( () => {
+		if ( ! id && isBlobURL( url ) ) {
+			setAttributes( {
 				url: undefined,
 				alt: '',
-			});
+			} );
 		}
-	}, []);
+	}, [] );
 
-	useEffect(() => {
-		if (isBlobURL(url)) {
-			setBlobURL(url);
+	useEffect( () => {
+		if ( isBlobURL( url ) ) {
+			setBlobURL( url );
 		} else {
-			revokeBlobURL(blobURL);
+			revokeBlobURL( blobURL );
 			setBlobURL();
 		}
-	}, [url]);
+	}, [ url ] );
+
+	useEffect( () => {
+		if ( url && ! prevURL ) {
+			titleRef.current.focus();
+		}
+	}, [ url, prevURL ] );
+
+	useEffect( () => {
+		if ( prevIsSelected && ! isSelected ) {
+			setSelectedLink();
+		}
+	}, [ isSelected, prevIsSelected ] );
 
 	return (
 		<>
 			<InspectorControls>
-				<PanelBody title={__('Image Settings', 'team-members')}>
-					{id && (
+				<PanelBody title={ __( 'Image Settings', 'team-members' ) }>
+					{ id && (
 						<SelectControl
-							label={__('Image Size', 'team-members')}
-							options={getImageSizeOptions()}
-							value={url}
-							onChange={onChangeImageSize}
+							label={ __( 'Image Size', 'team-members' ) }
+							options={ getImageSizeOptions() }
+							value={ url }
+							onChange={ onChangeImageSize }
 						/>
-					)}
-					{url && !isBlobURL(url) && (
+					) }
+					{ url && ! isBlobURL( url ) && (
 						<TextareaControl
-							label={__('Alt Text', 'team-members')}
-							value={alt}
-							onChange={onChangeAlt}
-							help={__(
+							label={ __( 'Alt Text', 'team-members' ) }
+							value={ alt }
+							onChange={ onChangeAlt }
+							help={ __(
 								"Alternative text describes your image to people can't see it. Add a short description with its key details.",
 								'team-members'
-							)}
+							) }
 						/>
-					)}
+					) }
 				</PanelBody>
 			</InspectorControls>
-			{url && (
+			{ url && (
 				<BlockControls group="inline">
 					<MediaReplaceFlow
-						name={__('Replace Image', 'team-members')}
-						onSelect={onSelectImage}
-						onSelectURL={onSelectURL}
-						onError={onUploadError}
+						name={ __( 'Replace Image', 'team-members' ) }
+						onSelect={ onSelectImage }
+						onSelectURL={ onSelectURL }
+						onError={ onUploadError }
 						accept="image/*"
-						allowedTypes={['image']}
-						mediaId={id}
-						mediaURL={url}
+						allowedTypes={ [ 'image' ] }
+						mediaId={ id }
+						mediaURL={ url }
 					/>
-					<ToolbarButton onClick={removeImage}>
-						{__('Remove Image', 'team-members')}
+					<ToolbarButton onClick={ removeImage }>
+						{ __( 'Remove Image', 'team-members' ) }
 					</ToolbarButton>
 				</BlockControls>
-			)}
-			<div {...useBlockProps()}>
-				{url && (
+			) }
+			<div { ...useBlockProps() }>
+				{ url && (
 					<div
-						className={`wp-block-blocks-course-team-member-img${
-							isBlobURL(url) ? ' is-loading' : ''
-						}`}
+						className={ `wp-block-blocks-course-team-member-img${
+							isBlobURL( url ) ? ' is-loading' : ''
+						}` }
 					>
-						<img src={url} alt={alt} />
-						{isBlobURL(url) && <Spinner />}
+						<img src={ url } alt={ alt } />
+						{ isBlobURL( url ) && <Spinner /> }
 					</div>
-				)}
+				) }
 				<MediaPlaceholder
 					icon="admin-users"
-					onSelect={onSelectImage}
-					onSelectURL={onSelectURL}
-					onError={onUploadError}
+					onSelect={ onSelectImage }
+					onSelectURL={ onSelectURL }
+					onError={ onUploadError }
 					accept="image/*"
-					allowedTypes={['image']}
-					disableMediaButtons={url}
-					notices={noticeUI}
+					allowedTypes={ [ 'image' ] }
+					disableMediaButtons={ url }
+					notices={ noticeUI }
 				/>
 				<RichText
-					placeholder={__('Member Name', 'team-member')}
+					ref={ titleRef }
+					placeholder={ __( 'Member Name', 'team-member' ) }
 					tagName="h4"
-					onChange={onChangeName}
-					value={name}
-					allowedFormats={[]}
+					onChange={ onChangeName }
+					value={ name }
+					allowedFormats={ [] }
 				/>
 				<RichText
-					placeholder={__('Member Bio', 'team-member')}
+					placeholder={ __( 'Member Bio', 'team-member' ) }
 					tagName="p"
-					onChange={onChangeBio}
-					value={bio}
-					allowedFormats={[]}
+					onChange={ onChangeBio }
+					value={ bio }
+					allowedFormats={ [] }
 				/>
+
+				<div className="wp-block-blocks-course-team-member-social-links">
+					<ul>
+						<DndContext
+							sensors={ sensors }
+							onDragEnd={ handleDragEnd }
+							modifiers={ [ restrictToHorizontalAxis ] }
+						>
+							<SortableContext
+								items={ socialLinks.map(
+									( item ) => `${ item.icon }-${ item.link }`
+								) }
+								strategy={ horizontalListSortingStrategy }
+							>
+								{ socialLinks.map( ( item, index ) => {
+									return (
+										<SortableItem
+											key={ `${ item.icon }-${ item.link }` }
+											id={ `${ item.icon }-${ item.link }` }
+											index={ index }
+											selectedLink={ selectedLink }
+											setSelectedLink={ setSelectedLink }
+											icon={ item.icon }
+										/>
+									);
+								} ) }
+							</SortableContext>
+						</DndContext>
+
+						{ isSelected && (
+							<li className="wp-block-blocks-course-team-member-add-icon-li">
+								<Tooltip
+									text={ __(
+										'Add Social Link',
+										'team-members'
+									) }
+								>
+									<button
+										aria-label={ __(
+											'Add Social Link',
+											'team-members'
+										) }
+										onClick={ addNewSocialItem }
+									>
+										<Icon icon="plus" />
+									</button>
+								</Tooltip>
+							</li>
+						) }
+					</ul>
+				</div>
+				{ selectedLink !== undefined && (
+					<div className="wp-block-blocks-course-team-member-link-form">
+						<TextControl
+							label={ __( 'Icon', 'text-members' ) }
+							value={ socialLinks[ selectedLink ].icon }
+							onChange={ ( icon ) => {
+								updateSocialItem( 'icon', icon );
+							} }
+						/>
+						<TextControl
+							label={ __( 'URL', 'text-members' ) }
+							value={ socialLinks[ selectedLink ].link }
+							onChange={ ( link ) => {
+								updateSocialItem( 'link', link );
+							} }
+						/>
+						<br />
+						<Button isDestructive onClick={ removeSocialItem }>
+							{ __( 'Remove Link', 'text-members' ) }
+						</Button>
+					</div>
+				) }
 			</div>
 		</>
 	);
 }
 
-export default withNotices(Edit);
+export default withNotices( Edit );
